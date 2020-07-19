e8239bb2e30c91c248d0cf8036a526155ecc62c7
davevarga666 <davevarga999@gmail.com>
2020-07-19 20:50:13 +0200

    Image loading animation added.

--- app/src/main/java/com/davevarga/latestnews/NewsRecyclerAdapter.kt
+++ app/src/main/java/com/davevarga/latestnews/NewsRecyclerAdapter.kt
@@ -4,8 +4,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.layout_news_list_item.*
 import kotlinx.android.synthetic.main.layout_news_list_item.view.*
 
 class NewsRecyclerAdapter(val items: List<NewsPost>) :
@@ -37,12 +39,20 @@ class NewsRecyclerAdapter(val items: List<NewsPost>) :
 
         fun bind(newsPiece: NewsPost) {
 
+            val circularProgressDrawable = CircularProgressDrawable(itemView.context)
+            circularProgressDrawable.strokeWidth = 5f
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.start()
+
+
             val requestOptions = RequestOptions()
                 .placeholder(R.drawable.ic_launcher_background)
                 .error(R.drawable.ic_launcher_background)
 
-            Glide.with(itemView.context)
+            GlideApp.with(itemView.context)
                 .applyDefaultRequestOptions(requestOptions)
+                .load("https://raw.githubusercontent.com/bumptech/glide/master/static/glide_logo.png")
+                .placeholder(circularProgressDrawable)
                 .load(newsPiece.urlToImage)
 //                .errorPlaceholder
                 .into(news_image)
