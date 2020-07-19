877fe593e4c25d504d679b3d46bcc4331314a7b7
davevarga666 <davevarga999@gmail.com>
2020-07-19 11:55:29 +0200

    MainActivity updated updated.

--- app/src/main/java/com/davevarga/latestnews/MainActivity.kt
+++ app/src/main/java/com/davevarga/latestnews/MainActivity.kt
@@ -2,51 +2,52 @@ package com.davevarga.latestnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var myNewsAdapter: NewsRecyclerAdapter
-    private var myNewsArrayList: ArrayList<NewsPost>? = null
-    private val BASE_URL = "https://newsapi.org/v2/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initRecyclerView()
-        addDataSet()
-        //loadData()
+        val request = ServiceBuilder.buildService(GetData::class.java)
+        val call = request.getData()
+
+        call.enqueue(object : Callback<RecentPosts>{
+            override fun onResponse(call: Call<RecentPosts>, response: Response<RecentPosts>) {
+                if (response.isSuccessful){
+                    recycler_view.apply {
+                        setHasFixedSize(true)
+                        layoutManager = LinearLayoutManager(this@MainActivity)
+                        adapter = NewsRecyclerAdapter(response.body()!!.results)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<RecentPosts>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
+            }
+
+//        initRecyclerView()
+
+    })
     }
 
-    private fun addDataSet(){
-        val data = DataSource.createDataSet()
-        myNewsAdapter.submitList(data)
-    }
+//    private fun initRecyclerView() {
+//
+//        recycler_view.apply {
+//            layoutManager = LinearLayoutManager(this@MainActivity)
+//            val topSpacingDecorator = TopSpacingItemDecoration(30)
+//            addItemDecoration(topSpacingDecorator)
+//            adapter = NewsRecyclerAdapter(items)
+//        }
+//    }
 
-    private fun initRecyclerView(){
-
-        recycler_view.apply {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            val topSpacingDecorator = TopSpacingItemDecoration(30)
-            addItemDecoration(topSpacingDecorator)
-            myNewsAdapter = NewsRecyclerAdapter()
-            adapter = myNewsAdapter
-        }
-    }
-
-    private fun loadData() {
-        val requestInterface = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .build().create(GetData::class.java)
-    }
 
 }
